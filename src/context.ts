51ae0b97f77220158f882ca49d346dfe611fe82a
Open Cipher <8209379+open-cipher@users.noreply.github.com>
2024-08-20 19:34:47 +0530

    feat: add more properties to artax context

--- src/context.ts
+++ src/context.ts
@@ -1,3 +1,9 @@
+import { Navigation } from './navigation';
+
 export interface ArtaxContext {
+  depth: number;
+  navigation: Navigation;
+  payloads: string[];
+  selectors: string[];
   uri: string;
 }
